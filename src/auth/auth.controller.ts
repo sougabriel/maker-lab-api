fa83f22105eab76be6d61eccbeeab780b64c79ff
Gabriel de Souza <110489605+sougabriel@users.noreply.github.com>
2024-09-24 15:35:21 -0300

    feat(auth): implement jwt validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
-import { AuthResquest } from './models/AuthRequest';
+import { AuthRequest } from './models/AuthRequest';
 
 @Controller()
 export class AuthController {
@@ -10,7 +10,7 @@ export class AuthController {
     @Post('login')
     @HttpCode(HttpStatus.OK)
     @UseGuards(LocalAuthGuard)
-    login(@Request() req: AuthResquest) {
+    login(@Request() req: AuthRequest) {
         return this.auth.login(req.user);
     }
 }
--- src/auth/models/AuthRequest.ts
+++ src/auth/models/AuthRequest.ts
@@ -1,6 +1,6 @@
 import { Request } from "express"
 import { User } from "src/user/entities/user.entity"
 
-export class AuthResquest extends Request {
+export class AuthRequest extends Request {
     user: User;
 }
