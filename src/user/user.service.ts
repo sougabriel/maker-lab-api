59e636b2ee9385dacedbe48bfd1d0b0a9819dc93
Gabriel de Souza <110489605+sougabriel@users.noreply.github.com>
2024-09-24 14:11:13 -0300

    feat(user): implement create

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all user`;
+  async create(createUserDto: CreateUserDto) {
+    const data = {
+      ...createUserDto,
+      password: await hash(createUserDto.password, 8)
+    };
+    const createdUser = await this.prisma.user.create({ data });
+    return {
+      ...createdUser,
+      password: undefined
+    };
   }
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }
