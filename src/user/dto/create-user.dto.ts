bc4537c75930ad6d66abe57e735943d3a734ba0a
Gabriel de Souza <110489605+sougabriel@users.noreply.github.com>
2024-10-15 19:59:19 -0300

    chore(user): change attributes

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,31 +1,49 @@
+import { Log } from 'src/log/entities/log.entity';
 import { User } from '../entities/user.entity';
 import {
-    IsBoolean,
-    IsEmail,
-    IsNumber,
-    IsOptional,
-    IsString,
-    Matches,
-    MaxLength,
-    MinLength,
+  IsBoolean,
+  IsEmail,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
 } from 'class-validator';
 
 export class CreateUserDto extends User {
-    @IsEmail()
-    email: string;
+  @IsEmail({}, { message: 'Invalid email! Try another!' })
+  email: string;
 
-    @IsString()
-    @MinLength(8)
-    @MaxLength(32)
-    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message: 'password too weak' })
-    password: string;
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
 
-    @IsOptional()
-    @IsNumber()
-    @MinLength(1)
-    accessLevel?: number;
+  @IsOptional()
+  @IsString({ message: 'Can not asign numbers as username!' })
+  @MinLength(5, {
+    message:
+      'too short! your username length must be between 5 and 24 characters!',
+  })
+  @MaxLength(24, {
+    message:
+      'too long! your username length must be between 5 and 24 characters!',
+  })
+  username?: string;
 
-    @IsOptional()
-    @IsBoolean()
-    isActive?: boolean;
+  @IsOptional()
+  @IsNumber()
+  @MinLength(1)
+  accessLevel?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+
+  @IsOptional()
+  logs?: Log[];
 }
