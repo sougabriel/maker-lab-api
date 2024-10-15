3f60bb606d440eea4b17ec80b50cc9a9d51f6e42
Gabriel de Souza <110489605+sougabriel@users.noreply.github.com>
2024-10-15 19:54:58 -0300

    feat(product): implement `product` endpoints methods

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private prisma: PrismaService) {}
+  
+  async create(data: CreateProductDto) {
+    return await this.prisma.product.create({ data });
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findAll() {
+    return await this.prisma.product.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    return await this.prisma.product.findUnique({ where: { id } });
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, data: UpdateProductDto) {
+    return await this.prisma.product.update({ data, where: { id } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async updateAll(data: UpdateProductDto) {
+    return await this.prisma.product.updateMany({ data });
+  }
+
+  async remove(id: number) {
+    return await this.prisma.product.delete({ where: { id } });
+  }
+
+  async removeAll() {
+    return await this.prisma.product.deleteMany();
   }
 }
