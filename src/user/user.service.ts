b2a61a37614fc84bce4502e2458ebfda3e1b7ce6
Gabriel de Souza <110489605+sougabriel@users.noreply.github.com>
2024-10-15 19:55:37 -0300

    feat(user): implement `user` update and delete methods

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { hash } from 'bcrypt';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -19,9 +20,32 @@ export class UserService {
     };
   }
 
-  findByEmail(email: string) {
-    return this.prisma.user.findUnique({
+  async findByEmail(email: string) {
+    return await this.prisma.user.findUnique({
       where: { email },
     })
   }
+
+  async update(email: string, data: UpdateUserDto) {
+    const user = await this.prisma.user.update({ 
+      data, 
+      where: { email } 
+    });
+    return {
+      ...user,
+      password: undefined
+    }
+  }
+
+  async delete(email: string) {
+    const user = await this.prisma.user.update({
+      data: {isActive: false},
+      where: { email }
+    });
+    return {
+      ...user,
+      password: undefined
+    }
+  }
+
 }
