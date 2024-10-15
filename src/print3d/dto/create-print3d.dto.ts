2157bd30945d945ca9edd17058a2626582909319
Gabriel de Souza <110489605+sougabriel@users.noreply.github.com>
2024-10-15 19:58:35 -0300

    chore(print3d): change attributes

--- src/print3d/dto/create-print3d.dto.ts
+++ src/print3d/dto/create-print3d.dto.ts
@@ -1,20 +1,16 @@
-import { IsNumber, IsString, MinLength } from "class-validator";
-import { Printer3d } from "src/printer3d/entities/printer3d.entity";
+import { IsNumber, IsOptional, IsString } from "class-validator";
 
 export class CreatePrint3dDto {
     @IsString()
     title: string;
 
+    @IsOptional()
     @IsString()
     printUrl?: string;
 
     @IsNumber()
-    @MinLength(0)
     price: number;
     
-    printer3D: Printer3d;
-    
     @IsNumber()
-    @MinLength(1)
     printer3DId: number;
 }
