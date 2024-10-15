870f201e0708dc530c09043b094212f0ad36591a
Gabriel de Souza <110489605+sougabriel@users.noreply.github.com>
2024-10-15 19:57:06 -0300

    chore(log): change attributes

--- src/log/dto/create-log.dto.ts
+++ src/log/dto/create-log.dto.ts
@@ -1,9 +1,6 @@
 import { IsDate, IsString } from "class-validator";
-import { User } from "src/user/entities/user.entity";
 
 export class CreateLogDto {
-    user: User;
-
     @IsString()
     userId: string;
     
