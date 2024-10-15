8b68287526a237ac40076c6d5a05c6df8acc04eb
Gabriel de Souza <110489605+sougabriel@users.noreply.github.com>
2024-10-15 19:54:45 -0300

    feat(printer3d): implement `printer3d` endpoints methods

--- src/printer3d/printer3d.service.ts
+++ src/printer3d/printer3d.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePrinter3dDto } from './dto/create-printer3d.dto';
 import { UpdatePrinter3dDto } from './dto/update-printer3d.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class Printer3dService {
-  create(createPrinter3dDto: CreatePrinter3dDto) {
-    return 'This action adds a new printer3d';
+  constructor(private prisma: PrismaService) {}
+  
+  async create(data: CreatePrinter3dDto) {
+    return await this.prisma.printer3D.create({ data });
   }
 
-  findAll() {
-    return `This action returns all printer3d`;
+  async findAll() {
+    return await this.prisma.printer3D.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} printer3d`;
+  async findOne(id: number) {
+    return await this.prisma.printer3D.findUnique({ where: { id } });
   }
 
-  update(id: number, updatePrinter3dDto: UpdatePrinter3dDto) {
-    return `This action updates a #${id} printer3d`;
+  async update(id: number, data: UpdatePrinter3dDto) {
+    return await this.prisma.printer3D.update({ data, where: { id } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} printer3d`;
+  async updateAll(data: UpdatePrinter3dDto) {
+    return await this.prisma.printer3D.updateMany({ data });
+  }
+
+  async remove(id: number) {
+    return await this.prisma.printer3D.delete({ where: { id } });
+  }
+
+  async removeAll() {
+    return await this.prisma.printer3D.deleteMany();
   }
 }
