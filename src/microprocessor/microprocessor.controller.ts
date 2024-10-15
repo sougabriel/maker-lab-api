62b970bd90df7ad14ec4753c1c68d4bc3e01b26d
Gabriel de Souza <110489605+sougabriel@users.noreply.github.com>
2024-10-15 19:58:04 -0300

    chore(microprocessor): change  attribute id modificator

--- src/microprocessor/microprocessor.controller.ts
+++ src/microprocessor/microprocessor.controller.ts
@@ -19,16 +19,16 @@ export class MicroprocessorController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.microprocessorService.findOne(+id);
+    return this.microprocessorService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateMicroprocessorDto: UpdateMicroprocessorDto) {
-    return this.microprocessorService.update(+id, updateMicroprocessorDto);
+    return this.microprocessorService.update(id, updateMicroprocessorDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.microprocessorService.remove(+id);
+    return this.microprocessorService.remove(id);
   }
 }
