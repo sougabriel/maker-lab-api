31da5eac4602359c4665fe13bfc21478284500a0
sougabriel <gabriel.dsouzapro@gmail.com>
2024-12-19 15:55:01 -0300

    test(app): create `CurrentUser` test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user/entities/user.entity';
+
+
+const user_test: User = {
+  email: 'test-user@email.com',
+  password: 'test123',
+  isActive: true
+}
 
 describe('AppController', () => {
   let appController: AppController;
@@ -17,6 +25,12 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+
+  describe('get me', () => {
+    it('should return "Hello test-user!"', () => {
+      expect(appController.getMe(user_test)).toBe(`Hello test-user!`);
     });
   });
 });
